df83c7bca3bedcec1859f292c09957d26689a44e
Anders Sven Axel Johansson <anderssvenaxelj@gmail.com>
2025-12-14 01:16:04 +0100

    Conditional rendering of movie credits rows. Simplify plural check for credits rolls.

--- src/components/MoviePageDetails.tsx
+++ src/components/MoviePageDetails.tsx
@@ -19,26 +19,11 @@ const MoviePageDetails = ({ movie }: { movie: Movie }) => {
     fetchCredits();
   }, [movie]);
 
-  const hasMultipleWriters = () => {
-    if (!credits) return false;
-    return (
-      credits?.crew.filter((crewMember) => crewMember.job === "Writer").length >
-      1
-    );
-  };
+  const writers = credits?.crew?.filter((c) => c.department === "Writing");
 
-  const hasMultipleDirectors = () => {
-    if (!credits) return false;
-    return (
-      credits?.crew.filter((crewMember) => crewMember.job === "Director")
-        .length > 1
-    );
-  };
+  const directors = credits?.crew?.filter((c) => c.job === "Director");
 
-  const hasMultipleActors = () => {
-    if (!credits) return false;
-    return credits?.cast.length > 1;
-  };
+  const cast = credits?.cast?.slice(0, 4);
 
   const addBulletSymbol = (index: number, arr: CrewMember[] | CastMember[]) => {
     return index < arr.length - 1 && <span>&nbsp;&nbsp;∙&nbsp;&nbsp;</span>;
@@ -64,56 +49,55 @@ const MoviePageDetails = ({ movie }: { movie: Movie }) => {
         <Col>
           <p>{movie.overview}</p>
 
-          <Row>
-            <Col xs="auto">{hasMultipleWriters() ? "Writers" : "Writer"}</Col>
-            <Col className="d-flex">
-              {credits?.crew
-                .filter((crewMember) => crewMember.department === "Writing")
-                .map((writer, index, writers) => (
+          {writers && writers.length > 0 && (
+            <Row>
+              <Col xs="auto">{writers.length > 1 ? "Writers" : "Writer"}</Col>
+              <Col className="d-flex">
+                {writers?.map((writer, index, writers) => (
                   <>
-                    <NavLink key={writer.id} href="">
+                    <NavLink key={writer.id} href={`/person/${writer.id}`}>
                       {writer.name}
                     </NavLink>
                     {addBulletSymbol(index, writers)}
                   </>
                 ))}
-            </Col>
-          </Row>
+              </Col>
+            </Row>
+          )}
 
-          <Row>
-            <Col xs="auto" className="d-flex">
-              {hasMultipleDirectors() ? "Directors" : "Director"}
-              {hasMultipleDirectors() ? "Directors" : "Director"}
-            </Col>
-            <Col className="d-flex">
-              {credits?.crew
-                .filter((crewMember) => crewMember.job === "Director")
-                .map((director, index, directors) => (
+          {directors && directors.length > 0 && (
+            <Row>
+              <Col xs="auto" className="d-flex">
+                {directors.length > 1 ? "Directors" : "Director"}
+              </Col>
+              <Col className="d-flex">
+                {directors?.map((director, index, directors) => (
                   <>
-                    <NavLink href="">{director.name}</NavLink>
+                    <NavLink href={`/person/${director.id}`}>
+                      {director.name}
+                    </NavLink>
                     {addBulletSymbol(index, directors)}
                   </>
                 ))}
-            <Col className="d-flex">
-              {credits?.crew
-                .filter((crewMember) => crewMember.job === "Director")
-                .map((director, index, directors) => (
+              </Col>
+            </Row>
+          )}
+
+          {cast && cast.length > 0 && (
+            <Row>
+              <Col xs="auto" className="d-flex">
+                {cast.length > 1 ? "Actors" : "Actor"}
+              </Col>
+              <Col className="d-flex">
+                {cast?.map((actor, index, actors) => (
                   <>
-                    <NavLink href="">{director.name}</NavLink>
-                    {addBulletSymbol(index, directors)}
+                    <NavLink href={`/person/${actor.id}`}>{actor.name}</NavLink>
+                    {addBulletSymbol(index, actors)}
                   </>
                 ))}
-            </Col>
-          </Row>
-          <Row>
-            <Col xs="auto" className="d-flex">
-              {hasMultipleActors() ? "Actors" : "Actor"}
-              {hasMultipleActors() ? "Actors" : "Actor"}
-            </Col>
-            <Col>
-              <NavLink href="">Anders</NavLink>
-            </Col>
-          </Row>
+              </Col>
+            </Row>
+          )}
         </Col>
       </Row>
     </div>
